02827dbde3384bfe9ece02c18963c39d580ae693
peddinti <vasanthabhanu94@gmail.com>
2024-01-15 16:29:29 +0000

    feat: add more problems from chapter-2

--- K_and_R/chapter_2/3_program.c
+++ K_and_R/chapter_2/3_program.c
@@ -1,7 +1,11 @@
 /* atoi example function implementation */
+/* A - 67 */
+/* a - 99 */
 #include <stdio.h>
 
 int atoi(char s[]);
+int lower(char c);
+int upper(char c);
 
 int main ()
 {
@@ -21,5 +25,32 @@ int atoi(char s[]) {
         n = 10 * n + (s[i] - '0');
     }
 
+    printf("lower of S is %c\n",lower('S'));
+    printf("upper of s is %c\n",upper('s'));
+
     return n;
+}
+
+int lower(char c) {
+
+    /*
+        if char c is >= 'a' and <= 'z', return c
+        if char c is >= 'A' and <= 'Z', return c + 32
+    */
+
+   if (c >= 'a' && c <= 'z') {
+       return c;
+   }
+
+    if (c >= 'A' && c <= 'Z') {
+        return (c + 'a' - 'A');
+    }
+
+}
+
+int upper(char c) {
+
+    if (c >= 'a' && c <= 'z')
+        return c - ('a' - 'A');
+    return c;
 }
